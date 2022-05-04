e054c92731d8617f54d9fd6e46c3df42208c98e4
unknown <arnaud.wist@gmail.com>
2022-05-05 00:25:36 +0200

    allumage des leds lors des onsets, ajout de fonction pour l'amplitude et l'interval

--- project_robot_dance/robot_dance/signals_processing.h
+++ project_robot_dance/robot_dance/signals_processing.h
@@ -3,9 +3,8 @@
 
 #define CHUNK_SIZE 	1024
 #define WINDOW_SIZE 64
-#define MIC_OUTPUT LEFT_OUTPUT
-#define LOW_FILTER_INDEX 8
-#define HIGH_FILTER_INDEX 256
+
+#include <hal.h>
 
 typedef struct {
     bool onset;
@@ -16,9 +15,11 @@ void processAudioData(int16_t *data, uint16_t num_samples);
 float* get_audio_buffer_ptr(void);
 float* get_rms_frequencies(void);
 float* get_auto_correlation(void);
-void wait_send_to_computer(void);
-uint16_t get_music_tempo(void);
+void wait_onset(void);
+uint8_t get_music_tempo(void);
+float get_music_interval(void);
 uint16_t get_music_pitch(void);
+uint16_t get_music_amplitude(void);
 
 
 
