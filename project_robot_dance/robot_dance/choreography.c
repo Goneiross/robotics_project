fc97bb29caaac0c369c2871ec086ce36c39447f9
Goneiross <guillaume.julien.pellerin@gmail.com>
2022-05-10 10:53:39 +0200

    Fix warnings

--- project_robot_dance/robot_dance/choreography.c
+++ project_robot_dance/robot_dance/choreography.c
@@ -31,7 +31,6 @@
 #define MOTOR_LOW_SPEED 500
 #define MOTOR_TURTLE_SPEED 100
 #define MOTOR_MIN_SPEED 100
-#define MOVE_NB 4
 
 #define SOUND_AMP_MIN 500
 
@@ -44,7 +43,6 @@
 
 #define TEMPO_SPEED_COEF 10
 
-#define MOTOR_SPEED_LIMIT   13 // [cm/s]
 #define NSTEP_ONE_TURN      1000 // number of step for 1 turn of the motor
 #define NSTEP_ONE_EL_TURN   4  //number of steps to do 1 electrical turn
 #define NB_OF_PHASES        4  //number of phases of the motors
